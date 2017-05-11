e45021d1fe7e90aee85ee96da9c15df3c9197355
Abrantex <matheus.abrantesc@gmail.com>
2017-05-10 23:32:53 -0300

    Update ler_rotulo.cpp

--- Teste/ler_rotulo.cpp
+++ Teste/ler_rotulo.cpp
@@ -6,25 +6,27 @@ using namespace std;
 
 int main () {
   	string line, rotulo;
-  	ifstream myfile ("retangulo.asm");
-  	ofstream tab_simb ("Ts.txt");
-  	int pos,i;
+  	ifstream myfile ("retangulo.asm");  //abre arquivo fonte
+  	ofstream tab_simb ("Ts.txt");        //abre tabela de simbolos
+  	int pos,i; //variaveis de controle
 
 
-  	if (!tab_simb.is_open() || ! myfile.is_open()){
+  	if (!tab_simb.is_open() || ! myfile.is_open()){  //verifica se arquivos foram abertos
   		cout<<"arquivo não abriu"<< endl;
   		return 0;
   	}
 
-  	while ( getline (myfile,line) ){
+  	while ( getline (myfile,line) ){  //laço que percorre o arquivo fonte recolhendo linhas
       	cout << line << '\n';
-      	pos = line.find(":");
+      	pos = line.find(":");     // pega posição do dois pontos (:)
+
+
       	if(pos >0){
-      		for(i=0;i<pos;i++){
+      		for(i=0;i<pos;i++){   //for pega todos os caracteres anteriores a : e salva na tabela de simbolos
       			rotulo = line.at(i);
-      			cout<<rotulo;
+      			tab_simb<<rotulo;
       		}
-      			cout<<endl;
+      			tab_simb<<endl;
 
 
 
