df49d49b319ea7fad5828ccae5b15914237afc97
lemossilva <guilhermelemos96@gmail.com>
2017-05-20 00:03:13 -0300

    Comentários no preprocessador

--- preprocessador.cpp
+++ preprocessador.cpp
@@ -10,47 +10,49 @@ using namespace std;
 
 bool fancy(ifstream &srce_file, fstream &no_comments)
 {
-	bool print;
-	string lines, line_temp;
+
+	string lines;
 	stringstream temp, temp1, temp2, temp3;
-	vector<string> equ_list, equ_cont_list;
-	vector<string>::iterator equ_list_it, equ_cont_list_it;
+	vector<string> equ_list;
+	vector<string>::iterator equ_list_it;
+
+
 	while(getline(srce_file,lines))
 	{
-		transform(lines.begin(),lines.end(), lines.begin(), ::toupper);
-		if (lines.find(";") != string::npos)
+		transform(lines.begin(),lines.end(), lines.begin(), ::toupper); //Coloca tudo em caixa alta
+		if (lines.find(";") != string::npos)						// Retira comentários
 		{
 			temp2 << lines.substr(0,int(lines.find(";"))) << endl;	
 		}
-		else
+		else					//Se não houver comentários, escreve a linha
 		{
 			temp2 << lines << endl;
 		}
 		//cout << temp2.str();
 		
 
-		if (temp2.str().find("EQU") != string::npos)
+		if (temp2.str().find("EQU") != string::npos)	//Acha linhas com a diretiva EQU
 		{
-			temp << temp2.str().substr(0,int(temp2.str().find(":")));
-			equ_list.push_back(temp.str());
-			temp.str("");
-			temp1 << temp2.str().substr((temp2.str().find("EQU")+4),temp2.str().find("\n"));
-			equ_list.push_back(temp1.str());
+			temp << temp2.str().substr(0,int(temp2.str().find(":")));	//Pega o label da linha com EQU
+			equ_list.push_back(temp.str());		//Salva o label no vector
+			temp.str("");						//Limpa a string temporária
+			temp1 << temp2.str().substr((temp2.str().find("EQU")+4),temp2.str().find("\n")); //Pega o valor do EQU
+			equ_list.push_back(temp1.str()); //Adiciona o valor do EQU na posição seguinte do label no vector
 			temp1.str("");
 		}
-		else if(!equ_list.empty())
+		else if(!equ_list.empty())			//Se a lista de EQU tiver algo, verifica se cada linha possui os labels já encontrados
 		{
-			for(equ_list_it = equ_list.begin();	equ_list_it != equ_list.end(); equ_list_it+=2)
+			for(equ_list_it = equ_list.begin();	equ_list_it != equ_list.end(); equ_list_it+=2) //Percorre de 2 em 2 pra procurar apenas os labels e não os valores
 			{	
 				if(lines.find(*equ_list_it) != string::npos)
 				{
-					temp3 << temp2.str().substr(0, temp2.str().find(*equ_list_it));
+					temp3 << temp2.str().substr(0, temp2.str().find(*equ_list_it)); //Remove o nome do label da string
 					temp2.str("");
 					temp2 << temp3.str();
 					temp3.str("");
-					advance(equ_list_it,1);
+					advance(equ_list_it,1);			//Substitui a label pelo valor definido na EQU
 					temp2 << *equ_list_it;
-					advance(equ_list_it,-1);
+					advance(equ_list_it,-1);		//Retorna o iterator para não quebrar o for e não dar segfault
 				}
 			}
 		}
@@ -59,13 +61,10 @@ bool fancy(ifstream &srce_file, fstream &no_comments)
 		temp2.str("");
 	}
 
-	for(equ_list_it = equ_list.begin();	equ_list_it != equ_list.end(); equ_list_it++)
-				cout << *equ_list_it; //PRINTAR A LISTA COM OS EQU
-	//cout << endl;
-	/*for(equ_cont_list_it = equ_cont_list.begin();	equ_cont_list_it != equ_cont_list.end(); equ_cont_list_it++)
-				cout << *equ_cont_list_it << " "; //PRINTAR A LISTA COM OS VALORES DOS EQU*/
+	/*for(equ_list_it = equ_list.begin();	equ_list_it != equ_list.end(); equ_list_it++)
+				cout << *equ_list_it; //PRINTAR A LISTA COM OS EQU */
 
-	return srce_file.eof() && no_comments.good();
+	return srce_file.eof() && no_comments.good(); //Se deu tudo certo, retorna true
 }
 
 int main(int argc, char const *argv[])
