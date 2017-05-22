2ae43acc4da951a7f927d877780d40e4ae8fee15
lemossilva <guilhermelemos96@gmail.com>
2017-05-22 20:21:24 -0300

    Adição de comentários na main e verificação de abertura de arquivos

--- Pasta_final/main.cpp
+++ Pasta_final/main.cpp
@@ -1,6 +1,10 @@
-//ESQUELETO DA MAIN PARA O MONTADOR/PREPROCESSSADOR
+/*
+##############################################
 
+FUNÇÃO MAIN PARA O PRÉ-PROCESSADOR E MONTADOR
 
+##############################################
+*/
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -20,7 +24,7 @@ fstream arquivo_saida, arquivo_pre_temp;
 int main(int argc, char const *argv[])
 {
 	string filename, option, saida, pre_temp;
-	if (!(argc == 4))
+	if (!(argc == 4))							//Checa se colocou o número correto de argummentos
 	{
 		cerr << "Número inválido de argumentos" << endl;
 		return -1;
@@ -28,28 +32,45 @@ int main(int argc, char const *argv[])
 	filename = argv[2];
 	option = argv[1];
 	saida = argv[3];
-	if ((filename.substr(filename.find(".")+1) == "asm") && option == "-o")
+	if ((filename.substr(filename.find(".")+1) == "asm") && option == "-o")  //Preprocessar e montar .asm
 	{
 		//Chama pré-processador e montador
-		cout << "ASM e -o" << endl;
 		arquivo_entrada.open(argv[2]);
+		if(!arquivo_entrada.is_open())
+		{
+			cerr << "Erro ao abrir arquivo" << endl;
+			return -1;
+		}
 		arquivo_pre_temp.open(argv[3],ios::out|ios::trunc);
+		if(!arquivo_pre_temp.is_open())
+		{
+			cerr << "Erro ao abrir arquivo" << endl;
+			return -1;
+		}
 		pre_proc(arquivo_entrada,arquivo_pre_temp);
 		chama_montador(saida,saida);
-		remove(argv[3]);
+		remove(argv[3]);								//Exclui arquivo intermediário
 	}
-	else if ((filename.substr(filename.find(".")+1) == "pre") && option == "-o")
+	else if ((filename.substr(filename.find(".")+1) == "pre") && option == "-o") //Apenas montar arquivo .pre
 	{
-		cout << "PRE e -o" << endl;
 		chama_montador(filename,saida);
 	}
-	else if((filename.substr(filename.find(".")+1) == "asm") && option == "-p")
+	else if((filename.substr(filename.find(".")+1) == "asm") && option == "-p") //Preprocessar arquivo .asm
 	{
-		cout << "ASM e -p" << endl;
 		string ext (".pre");
 		arquivo_entrada.open(argv[2]);
+		if(!arquivo_entrada.is_open())
+		{
+			cerr << "Erro ao abrir arquivo" << endl;
+			return -1;
+		}
 		saida += ext;
 		arquivo_pre_temp.open(saida.c_str(),ios::out|ios::trunc);
+		if(!arquivo_pre_temp.is_open())
+		{
+			cerr << "Erro ao abrir arquivo" << endl;
+			return -1;
+		}
 		pre_proc(arquivo_entrada,arquivo_pre_temp);
 	}
 	else
